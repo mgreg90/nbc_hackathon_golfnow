9b9bf12ebe0a3c2da924e5a5f75998b210debfbc
Michael Gregory <mgregory@carecloud.com>
2016-10-22 17:46:26 -0400

    slackbot test

--- app/models/golf_now.rb
+++ app/models/golf_now.rb
@@ -1,21 +1,24 @@
 class GolfNow < ApplicationRecord
     def self.tee_times_by_course_and_time_and_location(params)
         {
-            # speech: '',
-            # displayText: '',
+            speech: '',
+            displayText: '',
             text: 'yep'
-            # data: {
-            #     facebook: {
-            #         "attachment":{
-            #             "type":"image",
-            #             "payload":{
-            #                "url":"http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg"
-            #             }
-            #         }
-            #     }
-            # },
-            # contextOut: [],
-            # source: ''
+            data: {
+                facebook: {
+                    "attachment":{
+                        "type":"image",
+                        "payload":{
+                           "url":"http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg"
+                        }
+                    }
+                },
+                slack: {
+                    text: "hello!"
+                }
+            },
+            contextOut: [],
+            source: ''
         }.to_json
     end
     
