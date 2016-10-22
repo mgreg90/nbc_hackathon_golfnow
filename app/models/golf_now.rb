36a2949b27e9b8d85926f5b77a236a39cb2eceb7
Michael Gregory <mgregory@carecloud.com>
2016-10-22 17:40:02 -0400

    testing output

--- app/models/golf_now.rb
+++ app/models/golf_now.rb
@@ -1,21 +1,21 @@
 class GolfNow < ApplicationRecord
     def self.tee_times_by_course_and_time_and_location(params)
         {
-            speech: '',
-            displayText: '',
-            text: 'yep',
-            data: {
-                facebook: {
-                    "attachment":{
-                        "type":"image",
-                        "payload":{
-                           "url":"http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg"
-                        }
-                    }
-                }
-            },
-            contextOut: [],
-            source: ''
+            # speech: '',
+            # displayText: '',
+            text: 'yep'
+            # data: {
+            #     facebook: {
+            #         "attachment":{
+            #             "type":"image",
+            #             "payload":{
+            #                "url":"http://d39kbiy71leyho.cloudfront.net/wp-content/uploads/2016/05/09170020/cats-politics-TN.jpg"
+            #             }
+            #         }
+            #     }
+            # },
+            # contextOut: [],
+            # source: ''
         }.to_json
     end
     
