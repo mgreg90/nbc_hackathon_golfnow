1dc34fb2691748b0f7c40f340e6232a0ab68e624
Michael Gregory <mgregory@carecloud.com>
2016-10-23 06:04:45 -0400

    routes

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module NbcHackathonGolfnow
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += %W( #{config.root}/app/modules)
   end
 end
