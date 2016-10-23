951086a89e5bf4d846f3273b0bac723e3912c35c
Michael Gregory <mgregory@carecloud.com>
2016-10-23 05:03:07 -0400

    working on routes

--- app/controllers/api_dot_ai_controller.rb
+++ app/controllers/api_dot_ai_controller.rb
@@ -14,6 +14,7 @@ class ApiDotAiController < ApplicationController
   ]
   
   def action_handler
+    console.log "PARAMS: #{params}"
     HTTParty.post("#{BASE_URL}action_url", query: params) if valid_actions.include? action
   end
   
